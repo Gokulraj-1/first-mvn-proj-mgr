557cd703f1cc6a4774e7df020568a263746653fd
Gokulraj <110807453+Gokulraj-1@users.noreply.github.com>
2023-07-03 09:10:24 +0530

    day 20

---
+++ src/main/java/com/fssa/mgr/day19/solved/UserDetails.java
@@ -0,0 +1,41 @@
+package com.fssa.mgr.day19.solved;
+
+public  class UserDetails { // create a class
+	// initialize the variables
+	int id;
+	String name;
+	String password;
+	String email;
+
+	public int getId() { // create a method to get id
+		return id;
+	}
+
+	public void setId(int id) { // create a method to Set id
+		this.id = id;
+	}
+
+	public String getName() { // create a method to get name
+		return name;
+	}
+
+	public void setName(String name) { // create a method to Set name
+		this.name = name;
+	}
+
+	public String getPassword() { // create a method to get Password
+		return password;
+	}
+
+	public void setPassword(String password) { // create a method to Set Password
+		this.password = password;
+	}
+
+	public String getEmail() { // create a method to get Email
+		return email;
+	}
+
+	public void setEmail(String email) { // create a method to Set Email
+		this.email = email;
+	}
+}
---
+++ src/main/java/com/fssa/mgr/day19/solved/Validation.java
@@ -0,0 +1,71 @@
+package com.fssa.mgr.day19.solved;
+
+//import the scanner , regex matcher , regex pattern class from the package;
+import java.util.Scanner;
+
+
+public class Validation { // create a public class
+	public static void main(String[] args) { // main method starts
+		Scanner s2 = new Scanner(System.in);
+
+//		creating new object in the instance of class userDetails
+//		the user will have user id, user name, email id, password
+
+		UserDetails user = new UserDetails();
+
+//		getting the inputs from the user
+//		by trimming the start and end white spaces
+
+//		user id
+		System.out.print("Enter your Id(eg: 121,321): ");
+		int id = s2.nextInt();
+		s2.nextLine();
+
+//		name
+		System.out.print("Enter your name(eg: Vijay): ");
+		String name = s2.next();
+		s2.nextLine();
+
+//		email id
+		System.out.print("Enter your email id(eg: example@gmail.com): ");
+		String emailId = s2.next();
+		s2.nextLine();
+
+//		password
+		System.out.print("Enter your password: ");
+		String password = s2.next();
+		s2.nextLine();
+
+//		setting the values to the attributes of the user details
+
+		user.setId(id);
+		user.setName(name.trim());
+		user.setEmail(emailId.trim());
+		user.setPassword(password.trim());
+
+		try {
+
+//			calling the validate method to validate the user object details
+//			then the method will return true
+
+			boolean isValid = UserValidator.validate(user);
+
+//			if the isValid holds true then print the below pirnt statement
+
+			if (isValid) {
+
+				System.out.println("all are good");
+			}
+
+		} catch (IllegalArgumentException e) {
+
+			System.out.println(e.getMessage());
+		} finally {
+
+			System.out.println("Thank you");
+		}
+
+//		closing the scanner
+		s2.close();
+	} // main method ends
+}
